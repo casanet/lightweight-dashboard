fbd484d10039e153e061ebbef985ad626a5e0d85
Haim Kastner <haim.kastner@gmail.com>
2021-09-17 14:42:17 +0300

    Print env

--- scripts/set-environment.js
+++ scripts/set-environment.js
@@ -1,5 +1,10 @@
 const fs = require("fs");
 
+const API_URL = process.env.API_URL;
+console.log(`Setting env to ${API_URL}...`);
+
 fs.writeFileSync("./src/environments.json", JSON.stringify({
-  API_URL: `${process.env.API_URL || ''}/API`,
+  API_URL: `${API_URL || ''}/API`,
 }));
+
+console.log(`Setting env to ${API_URL} done`);
